c50c4787cb321f4f546c45faed7ad239bd911de5
Артем <templarka890@gmail.com>
2026-05-06 22:43:18 +0700

    Обновил игру, добавил логику комбо, цвета и поменял местами элементы

--- Stone.Scissors.Paper.py
+++ Stone.Scissors.Paper.py
@@ -5,24 +5,37 @@ root['bg'] = 'white'
 root.title('КБН')
 root.geometry('500x500')
 root.resizable(width=False, height=False)
-labelText = Label(root, text='', fg='black', font=('', 20))
-labelText.place(y = 200, x = 200)
+
+# Комбо
+player_combo_count = 0
+
+combo = 0
+
 # Логика Результатов
 rules = {
     'Ножницы': 'Бумага',
     "Камень": "Ножницы",
     "Бумага": "Камень"
 }
-# Функция выбора
 
+# Функция выбора
 def play_game(player_choice):
+    global combo
+    global result_text_label
     computer_choice = choice(['Камень', 'Ножницы', "Бумага"])
     if computer_choice == player_choice:
         result = 'Ничья'
+        result_text_label.configure(fg='orange')
     elif rules[player_choice] == computer_choice:
         result = 'Победа'
+        combo += 1 # При победе счетчки комбо возрастает
+        label_player_combo_count.configure(text=str(f'{combo}'))
+        result_text_label.configure(fg='green')
     else:
         result = 'Поражение'
+        combo = 0 # При поражении комбо обнуляется
+        label_player_combo_count.configure(text=str(f'{combo}'))
+        result_text_label.configure(fg='red')
     result_label.configure(text=result)
     computer_choice_label.configure(text=computer_choice)
 
@@ -34,7 +47,7 @@ stone = Button(root,
                activebackground= 'gray',
                command = lambda: play_game("Камень")
                )
-stone.place(x = 350, y = 50)
+
 
 scissors = Button(root,
                text='Ножницы',
@@ -43,7 +56,7 @@ scissors = Button(root,
                activebackground= 'gray',
                command = lambda: play_game("Ножницы")
                )
-scissors.place(x = 10, y = 50)
+
 
 paper = Button(root,
                text='Бумага',
@@ -52,11 +65,23 @@ paper = Button(root,
                activebackground= 'gray',
                command = lambda: play_game("Бумага")
                )
-paper.place(x = 200, y = 50)
-# Вывод результатов
-result_label = Label(root, text='', font= ('', 20))
-result_label.place(x = 180, y = 400)
-computer_choice_label = Label(root, text='', font=('', 20))
-computer_choice_label.place(x = 200, y = 200)
 
+# Создание объектов
+result_label = Label(root, text='', font= ('', 20))
+computer_choice_label = Label(root, text='', font=('', 30))
+label_player_combo_count = Label(root, text=f'{player_combo_count}', font=('',20), bg='white')
+result_text_label = Label(root, text='result', font=('', 20), bg='white', fg='blue')
+label_combo_text = Label(root, text='combo', font=('', 20), bg='white')
+
+
+# Вывод объектов
+result_label.place(x = 180, y = 100)
+computer_choice_label.place(x = 180, y = 200)
+label_player_combo_count.place(x=100,y=1)
+result_text_label.place(x=100,y=100)
+
+stone.place(x = 350, y = 327)
+scissors.place(x = 10, y = 327)
+paper.place(x = 200, y = 327)
+label_combo_text.place(x=1,y=1)
 root.mainloop() # работа программы
